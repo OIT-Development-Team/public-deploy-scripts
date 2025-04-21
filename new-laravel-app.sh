afbff6bd2d31be1869a13205ff85a33b48721875
mthenry <mikethenry@proton.me>
2025-04-21 16:17:52 -0500

    update

--- new-laravel-app.sh
+++ new-laravel-app.sh
@@ -220,9 +220,10 @@ EOL
     # Install Tailwind
     if $tailwind; then
         echo "Installing Tailwind..."
-        npm install -D tailwindcss postcss autoprefixer && npx tailwindcss init -p
-        curl https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/refs/heads/pint/tailwind.config.js -o tailwind.config.js
-        curl https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/refs/heads/pint/app.css -o resources/css/app.css
+        npm install -D tailwindcss postcss autoprefixer
+        npx tailwindcss init -p
+        curl https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/refs/tags/pint/tailwind.config.js -o tailwind.config.js
+        curl https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/refs/tags/pint/app.css -o resources/css/app.css
         echo "Installed Tailwind"
     fi
 
