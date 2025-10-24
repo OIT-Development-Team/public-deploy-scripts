7421fbf0eae4afc42db055b3e1498a79c11b39ed
mthenry <mikethenry@proton.me>
2025-10-24 11:30:48 -0500

    update

--- start-project.sh
+++ start-project.sh
@@ -19,22 +19,22 @@ done
 # 📥 Pull supporting files if missing
 # --------------------------------------
 [ ! -f .github/workflows/build.yaml ] && \
-    curl -sSL --create-dirs -o .github/workflows/build.yaml https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/master/build.yaml
+    curl -sSL --create-dirs -o .github/workflows/build.yaml https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/test/build.yaml
 
 [ ! -f .git/hooks/pre-commit ] && \
-    curl -sSL --create-dirs -o .git/hooks/pre-commit https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/master/laravel-hooks/pre-commit && chmod +x .git/hooks/pre-commit
+    curl -sSL --create-dirs -o .git/hooks/pre-commit https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/test/laravel-hooks/pre-commit && chmod +x .git/hooks/pre-commit
 
 [ ! -f docker-compose.yaml ] && \
-    curl -sSL -o docker-compose.yaml https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/master/docker-compose.yaml
+    curl -sSL -o docker-compose.yaml https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/test/docker-compose.yaml
 
 [ ! -f deploy-plan.json ] && \
-    curl -sSL -o deploy-plan.json https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/master/deploy-plan.json
+    curl -sSL -o deploy-plan.json https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/test/deploy-plan.json
 
 # --------------------------------------
 # 🗂️ Handle PV Option
 # --------------------------------------
 if echo "$FORWARD_ARGS" | grep -qw -- --pv; then
-	curl https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/master/add-pv.sh --create-dirs -o add-pv.sh
+	curl https://raw.githubusercontent.com/OIT-Development-Team/public-deploy-scripts/test/add-pv.sh --create-dirs -o add-pv.sh
 	chmod +x add-pv.sh
 	./add-pv.sh
 	rm add-pv.sh
